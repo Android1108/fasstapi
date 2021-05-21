97f14804a85bccf9d950885346d92a04fcea4285
Jayden <37990999+Android1108@users.noreply.github.com>
2021-05-21 14:04:34 +0800

    1

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+  uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
